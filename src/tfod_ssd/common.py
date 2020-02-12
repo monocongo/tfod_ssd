2ed3cf3700cf975a9e63853480aa2b7782aacc0a
James Adams <monocongo@gmail.com>
2020-02-12 11:23:21 -0500

    common detection inference and collection code moved into a common function named inference that can be used for both video and static images
    #5

--- src/tfod_ssd/common.py
+++ src/tfod_ssd/common.py
@@ -1,8 +1,11 @@
 import csv
 from typing import Dict, List
 
+import arrow
 import cv2
 import numpy as np
+
+from tfod_ssd.detector import ObjectDetectorTensorFlow
 
 # width of bounding boxes (in pixels)
 _BOX_LINE_WIDTH = 2
@@ -18,7 +21,7 @@ _LABEL_COLOR = (255, 0, 0)
 
 
 # ------------------------------------------------------------------------------
-def draw_boxes(
+def _draw_boxes(
         frame: np.ndarray,
         detections: List[Dict],
 ) -> np.ndarray:
@@ -58,6 +61,60 @@ def draw_boxes(
                             _LABEL_WIDTH)
 
     return frame
+
+
+# ------------------------------------------------------------------------------
+def inference(
+        detector: ObjectDetectorTensorFlow,
+        frame: np.ndarray,
+        confidence_threshold: float,
+) -> (np.ndarray, List[Dict]):
+    """
+    Performs inference on an image frame using an object detector model,
+    drawing bounding boxes with labels on the frame if any detections are
+    made and returning a list of dictionaries specifying the detections.
+
+    :param detector:
+    :param frame:
+    :return: the same image frame with bounding boxes drawn if any detections
+        were made and a list of dictionary objects specifying the detections
+    """
+
+    # get the date/time
+    time_stamp = arrow.utcnow().timestamp
+
+    # list of detections for this frame
+    detections_frame = []
+
+    # get the dimensions
+    (height, width) = frame.shape[:2]
+
+    # loop over the object detections
+    object_detections = detector.detect(frame, confidence_threshold)
+    for object_detection in object_detections:
+
+        # compute the detected object's bounding box (x, y)-coordinates
+        # NOTE bounding boxes are in order (y0, x0, y1, x1)
+        box = object_detection["bounding_box"] * np.array([height, width, height, width])
+        (start_y, start_x, end_y, end_x) = box.astype("int")
+
+        # build a record for the object detection, add to the list
+        detection = {
+            "time_stamp": time_stamp,
+            "class": object_detection["label"],
+            "confidence": f'{object_detection["probability"]:.2f}',
+            "start_x": start_x,
+            "start_y": start_y,
+            "end_x": end_x,
+            "end_y": end_y,
+        }
+        detections_frame.append(detection)
+
+    # draw bounding boxes on the frame to indicate detected objects
+    if len(detections_frame) > 0:
+        frame = _draw_boxes(frame, detections_frame)
+
+    return frame, detections_frame
 
 
 # ------------------------------------------------------------------------------
