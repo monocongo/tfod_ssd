98155b261ba4f85920223519ea385bbe9cfaabcd
James Adams <monocongo@gmail.com>
2020-02-11 20:50:38 -0500

    made resizing optional based on a constant
    #3

--- src/tfod_ssd/detect_video.py
+++ src/tfod_ssd/detect_video.py
@@ -17,6 +17,15 @@ from tfod_ssd.detector import ObjectDetectorTensorFlow
 # number of detections we'll batch together into a single write operation
 _DETECTIONS_BATCH_SIZE = 20
 
+# if positive then we'll resize video to this width
+_RESIZE_WIDTH = -1
+
+# width of bounding boxes (in pixels)
+_BOX_LINE_WIDTH = 2
+
+# color of bounding boxes
+_BOX_COLOR = (0, 255, 0)
+
 # ------------------------------------------------------------------------------
 # set up a basic, global logger object which will write to the console
 logging.basicConfig(
@@ -31,8 +40,6 @@ _logger = logging.getLogger(__name__)
 def _draw_boxes(frame: np.ndarray,
                 detections: List[Dict]) -> np.ndarray:
 
-    line_width = 2
-
     # for each detection draw a corresponding labeled bounding box
     for detection in detections:
 
@@ -42,8 +49,8 @@ def _draw_boxes(frame: np.ndarray,
                 frame,
                 (detection["start_x"], detection["start_y"]),
                 (detection["end_x"], detection["end_y"]),
-                (0, 255, 0),
-                2,
+                _BOX_COLOR,
+                _BOX_LINE_WIDTH,
             )
 
         # draw the object's label and probability value
@@ -129,7 +136,7 @@ def main():
     )
     args = vars(args_parser.parse_args())
 
-    # we're not storing detection records to database so we'll write detection info to CSV
+    # we'll write detection info to CSV using the following columns
     detection_field_names = [
         "time_stamp",
         "class",
@@ -144,9 +151,8 @@ def main():
     # makes it more likely that the keras-* models will use it
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-    # load the pre-trained, serialized object detection model
+    # load the pre-trained object detection model
     _logger.info("\n\n\tLoading object detection model...\n")
-
     object_detector = ObjectDetectorTensorFlow(args["labelmap"], args["fig"])
 
     # initialize the video stream, wait a few seconds to allow
@@ -163,10 +169,8 @@ def main():
 
     # get the initial frame, in order to have a baseline image for motion detection
     previous_frame = video_stream.read()
-    previous_frame = imutils.resize(previous_frame, width=400)
-
-    # perform some casts so we don't need to do them at each iteration
-    confidence_object = float(args["confidence"])
+    if _RESIZE_WIDTH > 0:
+        previous_frame = imutils.resize(previous_frame, width=_RESIZE_WIDTH)
 
     # a list of detection (dictionary) records -- when this list
     # fills up to or above the batch size then we'll write the
@@ -179,7 +183,8 @@ def main():
         # grab the frame from the threaded video stream and resize it
         # to have a maximum width of 400 pixels
         frame = video_stream.read()
-        frame = imutils.resize(frame, width=400)
+        if _RESIZE_WIDTH > 0:
+            frame = imutils.resize(frame, width=_RESIZE_WIDTH)
 
         # only perform detection if we've read a significantly different frame
         difference = np.sum(np.absolute(frame - previous_frame)) / np.size(frame)
@@ -198,7 +203,7 @@ def main():
             (height, width) = frame.shape[:2]
 
             # loop over the object detections
-            object_detections = object_detector.detect(frame, confidence_object)
+            object_detections = object_detector.detect(frame, args["confidence"])
             for object_detection in object_detections:
 
                 # compute the detected object's bounding box (x, y)-coordinates
