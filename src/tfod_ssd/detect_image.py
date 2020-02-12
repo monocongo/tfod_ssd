inference code moved into a common module (with fixed coordinate calculation)
#5

src/tfod_ssd/detect_image.py
@@ -7,7 +7,7 @@ import cv2
 import imutils
 import numpy as np
 
-from tfod_ssd.common import draw_boxes, write_detections
+from tfod_ssd.common import inference, write_detections
 from tfod_ssd.detector import ObjectDetectorTensorFlow
 
 # number of detections we'll batch together into a single write operation
@@ -121,41 +121,49 @@ def main():
         if _RESIZE_WIDTH > 0:
             frame = imutils.resize(frame, width=_RESIZE_WIDTH)
 
-        # get the date/time
-        time_stamp = arrow.utcnow().timestamp
+        # # get the date/time
+        # time_stamp = arrow.utcnow().timestamp
+        #
+        # # list of detections for this frame
+        # detections_frame = []
+        #
+        # # get the dimensions
+        # (height, width) = frame.shape[:2]
+        #
+        # # loop over the object detections
+        # object_detections = object_detector.detect(frame, args["confidence"])
+        # for object_detection in object_detections:
+        #
+        #     # compute the detected object's bounding box (x, y)-coordinates
+        #     # NOTE bounding boxes are in order (y0, x0, y1, x1)
+        #     box = object_detection["bounding_box"] * np.array([height, width, height, width])
+        #     (start_y, start_x, end_y, end_x) = box.astype("int")
+        #
+        #     # build a record for the object detection, add to the list
+        #     detection = {
+        #         "time_stamp": time_stamp,
+        #         "class": object_detection["label"],
+        #         "confidence": f'{object_detection["probability"]:.2f}',
+        #         "start_x": start_x,
+        #         "start_y": start_y,
+        #         "end_x": end_x,
+        #         "end_y": end_y,
+        #     }
+        #     detections_frame.append(detection)
+        #
+        # # draw bounding boxes on the frame to indicate detected objects
+        # if len(detections_frame) > 0:
+        #     frame = _draw_boxes(frame, detections_frame)
+        #
+        #     # add the detections from this frame into the current batch
+        #     detections_batch += detections_frame
 
-        # list of detections for this frame
-        detections_frame = []
+        # perform inference to get detections drawn on the frame
+        frame, detections = inference(object_detector, frame, args["confidence"])
 
-        # get the dimensions
-        (height, width) = frame.shape[:2]
-
-        # loop over the object detections
-        object_detections = object_detector.detect(frame, args["confidence"])
-        for object_detection in object_detections:
-
-            # compute the detected object's bounding box (x, y)-coordinates
-            box = object_detection["bounding_box"] * np.array([width, height, width, height])
-            (start_x, start_y, end_x, end_y) = box.astype("int")
-
-            # build a record for the object detection, add to the list
-            detection = {
-                "time_stamp": time_stamp,
-                "class": object_detection["label"],
-                "confidence": f'{object_detection["probability"]:.2f}',
-                "start_x": start_x,
-                "start_y": start_y,
-                "end_x": end_x,
-                "end_y": end_y,
-            }
-            detections_frame.append(detection)
-
-        # draw bounding boxes on the frame to indicate detected objects
-        if len(detections_frame) > 0:
-            frame = draw_boxes(frame, detections_frame)
-
-            # add the detections from this frame into the current batch
-            detections_batch += detections_frame
+        # add the detections from this frame into the current batch
+        if len(detections) > 0:
+            detections_batch += detections
 
         # we've now performed all detections (if any) on the frame
 